ef761af9ae08dc4d00126cc11376400052755b33
xavier <xavierhoskins@outlook.com>
2020-07-10 11:18:26 -0500

    2/5

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,23 @@
-def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
-
-    return None
+def get_indices_of_item_weights(weights, leng`th, limit):
+    weight = {}
+    dupe_check = False
+    duplicates = {}  # in order to pass test 2 we need to account for duplicate values
+    # iterate through weights and store them in the dict
+    for i in range(0, length):
+        current = weights[i]
+        weight[current] = i  # set the value to the index
+        # we subtract the current value from the limit to find which package
+        # combines to equal weight lim
+        target = limit - current
+        if target in weight:
+            if current > target:
+                return (i, weight[target])  # tuple of indexes
+            elif current < target:
+                return (i, weight[target])
+            elif target == current:  # if it finds a dupe
+                if dupe_check is False:  # and it isnt already clasified as a duplicate
+                    dupe_check = True  # change the bool
+                    duplicates[current] = i  # set the value to the index
+                elif dupe_check is True:
+                    return (i, duplicates[current])
+    return Non
